fcda7b9815dbc77e3c843954c8e3d4e27773ae81
kbalubam <kevin.baluba@hotmail.com>
2021-06-04 16:27:18 +0200

    vf sans detail

--- public/js/main.js
+++ public/js/main.js
@@ -14,43 +14,44 @@ let divCouleur= document.querySelector(".carreCouleur")
 
 w.addEventListener("click", ()=>{
     divCouleur.style.backgroundColor = "grey"
-})
+});
 
 r.addEventListener("click", ()=>{
     divCouleur.style.backgroundColor = "tomato"
-})
+});
 
 g.addEventListener("click", ()=>{
     divCouleur.style.backgroundColor = "green"
-})
+});
 
 b.addEventListener("click", ()=>{
     divCouleur.style.backgroundColor = "blue"
-})
+});
 
 solid.addEventListener("click", ()=>{
     divCouleur.style.border = "2px solid black"
-})
+});
 
 dotted.addEventListener("click", ()=>{
     divCouleur.style.border = "2px dotted black"
-})
+});
 
 fine.addEventListener("click", ()=>{
     divCouleur.style.borderLeft = "1px solid black";
     divCouleur.style.borderRight = "1px solid black";
-    divCouleur.style.borderBottom = "1px solid black"
-})
+    divCouleur.style.borderBottom = "1px solid black";
+    divCouleur.style.borderTop ="none"
+});
 
 topFIne.addEventListener("click", ()=>{
     divCouleur.style.borderTop = "2px solid red"
-})
+});
 topNormal.addEventListener("click", ()=>{
     divCouleur.style.borderTop = "4px solid red"
-})
+});
 topEpais.addEventListener("click", ()=>{
     divCouleur.style.borderTop = "8px solid red"
-})
+});
 
 let inputColor = document.querySelector(".barrecoleur").firstElementChild;
 let btn = inputColor.nextElementSibling;
@@ -58,54 +59,60 @@ let btn = inputColor.nextElementSibling;
 
 btn.addEventListener("click", ()=>{
     divCouleur.style.backgroundColor = inputColor.value;
-})
+});
 
 let inputBorder = document.querySelectorAll("input")[1];
 
 inputBorder.addEventListener("click", ()=>{
     divCouleur.style.border=`${inputBorder.value}px solid black`;
-})
+});
 let inputBorderRight = document.querySelectorAll("input")[2];
 
 inputBorderRight.addEventListener("click", ()=>{
     divCouleur.style.borderRight=`${inputBorderRight.value}px solid black`;
-})
+});
 let inputBorderLeft = document.querySelectorAll("input")[3];
 
 inputBorderLeft.addEventListener("click", ()=>{
     divCouleur.style.borderLeft=`${inputBorderLeft.value}px solid black`;
-})
+});
 let inputBorderTop = document.querySelectorAll("input")[4];
 
 inputBorderTop.addEventListener("click", ()=>{
     divCouleur.style.borderTop=`${inputBorderTop.value}px solid black`;
-})
+});
 let inputBorderBottom = document.querySelectorAll("input")[5];
 
 inputBorderBottom.addEventListener("click", ()=>{
     divCouleur.style.borderBottom=`${inputBorderBottom.value}px solid black`;
-})
+});
 
 
 let inputRadius = document.querySelectorAll("input")[6];
+let inputRadiusRB = document.querySelectorAll("input")[7];
+let inputRadiusBR = document.querySelectorAll("input")[8];
+let inputRadiusLB = document.querySelectorAll("input")[9];
+let inputRadiusTL = document.querySelectorAll("input")[10];
 
 inputRadius.addEventListener("click", ()=>{
     divCouleur.style.borderRadius = `${inputRadius.value}px`
-})
-let inputRadiusR = document.querySelectorAll("input")[7];
+});
+inputRadiusTL.addEventListener("click", ()=>{
+    divCouleur.style.borderRadius = ` ${inputRadiusTL.value}px ${inputRadiusRB.value}px ${inputRadiusBR.value}px ${inputRadiusLB.value}px`
+});
 
-inputRadiusR.addEventListener("click", ()=>{
-    divCouleur.style.borderRadius = ` 0px ${inputRadiusR.value}px 0px 0px`
-})
-let inputRadiusB = document.querySelectorAll("input")[7];
+inputRadiusRB.addEventListener("click", ()=>{
+    divCouleur.style.borderRadius = ` ${inputRadiusTL.value}px ${inputRadiusRB.value}px ${inputRadiusBR.value}px ${inputRadiusLB.value}px`
+});
 
-inputRadiusB.addEventListener("click", ()=>{
-    divCouleur.style.borderRadius = ` 0px 0px ${inputRadiusL.value}px 0px`
-})
-let inputRadiusB = document.querySelectorAll("input")[7];
+inputRadiusBR.addEventListener("click", ()=>{
+    divCouleur.style.borderRadius = ` ${inputRadiusTL.value}px ${inputRadiusRB.value}px ${inputRadiusBR.value}px ${inputRadiusLB.value}px`
+});
 
-inputRadiusB.addEventListener("click", ()=>{
-    divCouleur.style.borderRadius = ` 0px 0px ${inputRadiusL.value}px 0px`
-})
+inputRadiusLB.addEventListener("click", ()=>{
+    divCouleur.style.borderRadius = `${inputRadiusTL.value}px ${inputRadiusRB.value}px ${inputRadiusBR.value}px ${inputRadiusLB.value}px`
+});
+
+
 
 
